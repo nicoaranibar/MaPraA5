a633861152879c91d2573f3c89f0a7cc2dc440ff
nicoaranibar <nicolasaranibar100@gmail.com>
2025-07-02 14:58:48 +0200

    Graph reading method implemented

--- src/a_stern.cpp
+++ src/a_stern.cpp
@@ -2,6 +2,7 @@
 #include "astern/text_visualizer.h"
 #include "astern/unit.h"
 #include <algorithm>
+#include <iostream>
 
 // Ein Graph, der Koordinaten von Knoten speichert.
 class CoordinateGraph : public DistanceGraph {
@@ -11,6 +12,33 @@ class CoordinateGraph : public DistanceGraph {
   CostT estimatedCost(VertexT from, VertexT to) const override;
 
   CostT cost(VertexT from, VertexT to) const override;
+
+  friend std::istream& operator>>(std::istream& is, CoordinateGraph& g) {
+    size_t num_edges;
+    is >> g.vertexCount >> num_edges;
+    g.adjacency_list.assign(g.vertexCount, {});
+    g.coordinates.resize(g.vertexCount);
+
+    for (size_t v = 0; v < num_edges; ++v) {
+      VertexT from, to;
+      CostT cost;
+      is >> from >> to >> cost;
+      g.adjacency_list[from].emplace_back(to, cost);
+    }
+
+    for (VertexT v = 0; v < g.vertexCount; ++v) {
+      double x, y;
+      is >> x >> y;
+      g.coordinates[v] = {x, y};
+    }
+
+    return is;
+  }
+
+private:
+  std::vector<NeighborT> adjacency_list;
+  std::vector<std::pair<double, double>> coordinates; 
+
 };
 
 void Dijkstra(const DistanceGraph& g, /* GraphVisualizer& v, */ VertexT start,
@@ -20,7 +48,7 @@ void Dijkstra(const DistanceGraph& g, /* GraphVisualizer& v, */ VertexT start,
   for (VertexT i = 0; i < g.numVertices(); ++i) {
     not_visited[i] = i;
   }
-  not_visited.erase(std::remove(not_visited.begin(), not_visited.end(), start), not_visited.end());
+  not_visited.erase(std::find(not_visited.begin(), not_visited.end(), start));
 
   // Initialize costs
   kostenZumStart.assign(g.numVertices(), infty);
@@ -42,14 +70,13 @@ void Dijkstra(const DistanceGraph& g, /* GraphVisualizer& v, */ VertexT start,
     if (min_index == infty) {
       break;  // No more reachable vertices
     }
-    not_visited.erase(std::remove(not_visited.begin(), not_visited.end(), min_index), not_visited.end());
+    not_visited.erase(std::find(not_visited.begin(), not_visited.end(), min_index));
 
     for (const auto& neighbor : g.getNeighbors(min_index)) {
       kostenZumStart[neighbor.first] =
           std::min(kostenZumStart[neighbor.first], kostenZumStart[min_index] + neighbor.second);
     }
   }
-
 }
 
 bool A_star(const DistanceGraph& g, GraphVisualizer& v, VertexT start,
